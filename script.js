d990f15487276e978e76add21c6ec0605d8d74bc
Jason Toups <jason.p.toups@gmail.com>
2018-04-16 15:37:40 -0400

    create candidate cards with full rendering functionality

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ Vue.component('filter-header', {
 
 Vue.component('filter-item', {
   template: `
-  <li><a :class="{'is-active': clicked }" @click.prevent="clicked = !clicked">
+  <li><a :class="{'is-active': clicked }" @click.prevent="isClicked">
     <slot></slot>
   </a></li>
   `,
@@ -58,9 +58,74 @@ Vue.component('filter-item', {
     return {
       clicked: false
     }
+  },
+  methods: {
+    isClicked: function (event) {
+      this.clicked = !this.clicked
+    }
+  }
+})
+
+Vue.component('candidate-card', {
+  props: {
+    candidate: {
+      type: Object
+    }
+  },
+  template: `
+  <div class="card">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-content">
+        <p class="title is-4">{{candidate.candidate.name}} ({{candidate.candidate.state}}-{{candidate.candidate.district}})</p>
+      </div>
+    </div>
+    <div class="content">
+      <p><span>Gender:</span> {{candidate.candidate.gender}}</p>
+      <p><span>Ethnicity:</span> {{this.showEthnicity()}}</p>
+      <p><span>Sexuality:</span> {{candidate.candidate.sexuality}}</p>
+      <p><span>Professions:</span> {{this.showProfessions()}}</p>
+      <a href="#">Website</a> 
+    </div>
+  </div>
+  </div>
+  `,
+  methods: {
+    showEthnicity () {
+      return _.keys(_.pickBy(this.ethnicities)).toString()
+    },
+    showProfessions () {
+      return _.keys(_.pickBy(this.professions)).toString()
+    }
+  },
+  computed: {
+    ethnicities () {
+      return this.candidate.candidate.ethnicities
+    },
+    professions () {
+      return this.candidate.candidate.professions
+    }
   }
 })
 
 var app = new Vue({
-  el: '#root'
+  el: '#root',
+  data() {
+    return {
+      candidates: [],
+      url: 'http://localhost:4000/api/candidates'
+    }
+  },
+  beforeMount:
+    function () {
+      fetch(this.url).then(res => res.json())
+        .then(res => {
+          this.candidates = res
+        })
+    }
 })
